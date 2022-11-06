415652c4d7c9ca3e55e7349831ed0c9ed8b1cf3b
Per Edward Nilsen <nilliz85@gmail.com>
2022-11-06 20:41:15 +0100

    added some styling

--- js/question-2.js
+++ js/question-2.js
@@ -1,38 +1,33 @@
 const apiKey = "c93a32fa9b9b4e0c9d07d940c01229e6";
 const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=";
-
-const gamesContainer = document.querySelector(".games");
+const gameList = document.querySelector(".games-list");
 const loading = document.querySelector(".loading");
 
 async function getGames() {
   try {
     debugger;
     const response = await fetch(url + apiKey);
-    console.log(response);
-    loading.innerHTML = "Loading..."
+    loading.innerHTML = `<h1>LOADING...</h1>`;
     const data = await response.json();
-    const result = data.result;
-
+    const result = data.results;
     return result;
   } catch (error) {
     console.log("Error");
   }
 }
-
-async function listOfGames(){
-  getGames();
+async function listOfGames() {
+  getGames()
     .then((data) => {
-      loading.innerHTML= "";
+      loading.innerHTML = `<h2></h2>`;
       return data;
     })
     .then((data) => {
-      for (let i = 0; i < 8; i++){
-        const gameName = data[i].name;
-        const gameRating = data[i].rating;
-        const gameTagsLength = data[i].tags.length;
-
-        gamesContainer.innerHTML += `<h2>${gameName}</h2><h3>${gameRating}</h3><h3>${gameTagsLength}</h3>`;
+      for (let i = 0; i < 8; i++) {
+        const name = data[i].name;
+        const rating = data[i].rating;
+        const tagLength = data[i].tags.length;
+        gameList.innerHTML += `<div class="games"><h2 class="name" >${name}</h2><h3 class="rating">Rating: ${rating}</h3><h3 class="tag">Length of tag: ${tagLength}</h3></div>`;
       }
-    })
-
+    });
 }
+listOfGames();
